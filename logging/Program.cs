128a4d2425543490c4f717cf07bc87a706c49e35
James Liu <jliu@sirokygroup.com>
2023-06-14 17:25:22 -0400

    configure

--- logging/Program.cs
+++ logging/Program.cs
@@ -2,11 +2,14 @@
 
 
 using logging.CustomLogging;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
-using IHost host = Host.CreateDefaultBuilder(args)
+var hostBuilder = Host.CreateDefaultBuilder(args)
+   .ConfigureAppConfiguration(configBuilder => configBuilder.AddJsonFile("appsettings.json",false,true))
    .ConfigureLogging(logBuilder =>
         logBuilder.ClearProviders()
            .AddColorConsoleLogger(configuration => {
@@ -14,8 +17,14 @@ using IHost host = Host.CreateDefaultBuilder(args)
                configuration.LogLevelToColorMap[LogLevel.Warning] = ConsoleColor.DarkCyan;
                // Replace warning value from appsettings.json of "Red"
                configuration.LogLevelToColorMap[LogLevel.Error] = ConsoleColor.DarkRed;
-    }))
-    .Build();
+           }))
+   .ConfigureServices((hostBuilderContext,services) => {
+       services.AddOptions().Configure<LoggingDatabase>(hostBuilderContext.Configuration.GetSection("LoggingDatabase"));
+       services.AddScoped<Test1>();
+   });
+
+
+using IHost host  = hostBuilder.Build();
 
 
 var logger = host.Services.GetRequiredService<ILogger<Program>>();
@@ -25,6 +34,10 @@ logger.LogInformation(3, "Nothing to see here."); // Logs in ConsoleColor.DarkGr
 logger.LogWarning(5, "Warning... that was odd."); // Logs in ConsoleColor.DarkCyan
 logger.LogError(7, "Oops, there was an error.");  // Logs in ConsoleColor.DarkRed
 logger.LogTrace(5, "== 120.");                    // Not logged
+
+
+var test1 = host.Services.GetRequiredService<Test1>();
+test1.Test();
 
 await host.RunAsync();
 
@@ -58,7 +71,11 @@ await host.RunAsync();
 
 
 
-
+class LoggingDatabase {
+    public string? DbName { get; set; } 
+    public int Port { get; set; }
+    public string? UserName { get; set; }
+}
 
 class Secret {
     private readonly string secret = "abc980WF";
@@ -71,11 +88,22 @@ class Secret {
 internal class Test1 {
 
     private readonly ILogger<Test1> _logger;
-
-    public Test1(ILogger<Test1> logger) {
+    private readonly IOptionsMonitor<LoggingDatabase> loggingDatabaseOptions;
+    public Test1(ILogger<Test1> logger, IOptionsMonitor<LoggingDatabase> options) {
+        this.loggingDatabaseOptions = options;
         _logger = logger;
+
+
+        options.OnChange(loggingDb => {
+            Console.WriteLine(loggingDb.DbName);
+        });
+
+        Console.WriteLine(this.loggingDatabaseOptions.CurrentValue.DbName + " in Test1 constructor");
+
+       
     }
 
+    public IOptionsMonitor<LoggingDatabase> Options { get; }
 
     public void Test() {
         try {
