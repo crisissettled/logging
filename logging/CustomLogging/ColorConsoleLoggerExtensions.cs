00f26bef4858f7c70d43bc7392e44c5df33ddca8
James Liu <jliu@sirokygroup.com>
2023-06-25 14:05:57 -0400

    adjust

--- logging/CustomLogging/ColorConsoleLoggerExtensions.cs
+++ logging/CustomLogging/ColorConsoleLoggerExtensions.cs
@@ -22,7 +22,9 @@ namespace logging.CustomLogging {
         }
 
         public static ILoggingBuilder AddColorConsoleLogger(this ILoggingBuilder builder, IConfiguration config) {
-            builder.AddColorConsoleLogger();
+            //builder.AddColorConsoleLogger();
+            //builder.Services.AddSingleton<ILoggerProvider, ColorConsoleLoggerProvider>();     
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, ColorConsoleLoggerProvider>()); 
             builder.Services.Configure<ColorConsoleLoggerConfiguration>(config);
 
             return builder;
