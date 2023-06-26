d8ed79752d473a892c74bff74a35396db3ed3010
James Liu <jliu@sirokygroup.com>
2023-06-26 07:03:37 -0400

    adjust

--- logging/CustomLogging/ColorConsoleLogger.cs
+++ logging/CustomLogging/ColorConsoleLogger.cs
@@ -16,7 +16,7 @@ namespace logging.CustomLogging {
 
         public IDisposable? BeginScope<TState>(TState state) where TState : notnull => default!;
 
-        public bool IsEnabled(LogLevel logLevel) => _getCurrentConfig().LogLevelToColorMap.ContainsKey(logLevel);
+        public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;  //_getCurrentConfig().LogLevelToColorMap.ContainsKey(logLevel);
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) {
 
@@ -28,17 +28,19 @@ namespace logging.CustomLogging {
             if (config.EventId == 0 || config.EventId == eventId.Id) {
                 ConsoleColor originalColor = Console.ForegroundColor;
 
-                Console.ForegroundColor = config.LogLevelToColorMap[logLevel];
-                Console.WriteLine($"[{eventId.Id,2}: {logLevel,-12}]");
+                if (config.LogLevelToColorMap.ContainsKey(logLevel)) {
+                    Console.ForegroundColor = config.LogLevelToColorMap[logLevel];
+                    Console.WriteLine($"[{eventId.Id,2}: {logLevel,-12}]");
 
-                Console.ForegroundColor = originalColor;
-                Console.Write($"     {_name} - ");
+                    Console.ForegroundColor = originalColor;
+                    Console.Write($"     {_name} - ");
 
-                Console.ForegroundColor = config.LogLevelToColorMap[logLevel];
-                Console.Write($"{formatter(state, exception)}");
+                    Console.ForegroundColor = config.LogLevelToColorMap[logLevel];
+                    Console.Write($"{formatter(state, exception)}");
 
-                Console.ForegroundColor = originalColor;
-                Console.WriteLine();
+                    Console.ForegroundColor = originalColor;
+                    Console.WriteLine();
+                }
             }
         }
     }
